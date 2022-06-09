f732a1647becd8b75199028bfb191c24e7715002
War777 <oscarResendizUaq@gmail.com>
2022-06-09 12:43:47 -0500

    breaking bad API

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+import Logo from '../img/logo.png'
+
+
+
+
+const Header = () => {
+    
+
+
+  return (
+    <header className='center'>
+        <img 
+        src={Logo}
+        alt=''
+        />
+    </header>
+  )
+}
+
+export default Header
